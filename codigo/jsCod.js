c635fad5bf521805a347f67628b02acff3178fc1
Euclides Bento Cusso <tecnoinovacao200@gmail.com>
2025-06-28 23:16:07 +0000

    melhorou

---
+++ codigo/jsCod.js
@@ -0,0 +1,21 @@
+
+function clicou1(){
+    let botao_1 = document.getElementById("princi_1")
+    botao_1.style.display = "block"
+}
+
+function clicou2(){
+    let botao_2 = document.getElementById("princi_2")
+    botao_2.style.display = "block"
+}
+
+function clicou3(){
+    let botao_3 = document.getElementById("princi_3")
+    botao_3.style.display = "block"
+}
+
+function clicou4(){
+    let botao_4 = document.getElementById("princi_4")
+    botao_4.style.display = "block"
+}
+
