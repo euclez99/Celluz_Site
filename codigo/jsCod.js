5943e65c7b4311ca889179abdb7746bbd100eef5
Euclides Bento Cusso <tecnoinovacao200@gmail.com>
2025-06-29 16:47:45 +0000

    ate aqui melhorou
    criamos css e javaScript

--- codigo/jsCod.js
+++ codigo/jsCod.js
@@ -4,40 +4,82 @@ let botao_2 = document.getElementById("princi_2")
 let botao_3 = document.getElementById("princi_3")
 let botao_4 = document.getElementById("princi_4")
 
+let botao_5 = document.getElementById("subio1")
+let botao_6 = document.getElementById("subio2")
+let botao_7 = document.getElementById("subio3")
+let botao_8 = document.getElementById("subio4")
+
+let botao_9 = document.getElementById("desceu1")
+let botao_10 = document.getElementById("desceu2")
+let botao_11 = document.getElementById("desceu3")
+let botao_12 = document.getElementById("desceu4")
+
 
 
 function clicou1(){
     botao_1.style.display = "block"
+    
+    botao_5.style.display = "none"
+
+    botao_9.style.display = "block"
 }
 
 function clicou2(){
     botao_2.style.display = "block"
+
+    botao_6.style.display = "none"
+
+    botao_10.style.display = "block"
 }
 
 function clicou3(){
     botao_3.style.display = "block"
+    
+    botao_7.style.display = "none"
+
+    botao_11.style.display = "block"
 }
 
 function clicou4(){
     botao_4.style.display = "block"
+    
+    botao_8.style.display = "none"
+
+    botao_12.style.display = "block"
 }
 
 
 
 function vazou1(){
     botao_1.style.display = "none"
+
+    botao_5.style.display = "block"
+
+    botao_9.style.display = "none"
 }
 
 function vazou2(){
     botao_2.style.display = "none"
+    
+    botao_6.style.display = "block"
+
+    botao_10.style.display = "none"
 }
 
 function vazou3(){
     botao_3.style.display = "none"
+    
+    botao_7.style.display = "block"
+
+    botao_11.style.display = "none"
 }
 
 function vazou4(){
     botao_4.style.display = "none"
+    
+    botao_8.style.display = "block"
+
+    botao_12.style.display = "none"
 }
 
 
