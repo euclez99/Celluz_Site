b3d5c6dc0e834b7b68d5d3bcf95da79900957cd0
Euclides Bento Cusso <tecnoinovacao200@gmail.com>
2025-06-29 00:08:52 +0000

    melhorou2

--- codigo/jsCod.js
+++ codigo/jsCod.js
@@ -1,21 +1,45 @@
 
+let botao_1 = document.getElementById("princi_1")
+let botao_2 = document.getElementById("princi_2")
+let botao_3 = document.getElementById("princi_3")
+let botao_4 = document.getElementById("princi_4")
+
+
+
 function clicou1(){
-    let botao_1 = document.getElementById("princi_1")
     botao_1.style.display = "block"
 }
 
 function clicou2(){
-    let botao_2 = document.getElementById("princi_2")
     botao_2.style.display = "block"
 }
 
 function clicou3(){
-    let botao_3 = document.getElementById("princi_3")
     botao_3.style.display = "block"
 }
 
 function clicou4(){
-    let botao_4 = document.getElementById("princi_4")
     botao_4.style.display = "block"
 }
+
+
+
+function vazou1(){
+    botao_1.style.display = "none"
+}
+
+function vazou2(){
+    botao_2.style.display = "none"
+}
+
+function vazou3(){
+    botao_3.style.display = "none"
+}
+
+function vazou4(){
+    botao_4.style.display = "none"
+}
+
+
+
 
