996841bc0fe4a77602e2c2ab0dc70a0aacf60c15
Euclides Bento Cusso <tecnoinovacao200@gmail.com>
2025-07-04 23:19:23 +0000

    alterandoTudo
    novoModelo

--- codigo/js.js
+++ codigo/js.js
@@ -1,11 +1,19 @@
 function Clicou() {
-    let links = document.getElementById("out")
+    let links = document.getElementById("diferente")
     links.style.display = "block"
     
 }
 
+
+
 function moveu() {
-    let oud = document.getElementById("out")
+    let oud = document.getElementById("diferente")
+    oud.style.display = "none"
+    
+}
+
+function moveu() {
+    let oud = document.getElementById("diferente")
     oud.style.display = "none"
     
 }
