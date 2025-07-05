52cb623dbc3edd793e295ad2f0c5a805a17b56cd
Euclides Bento Cusso <tecnoinovacao200@gmail.com>
2025-07-05 22:54:43 +0000

    melhoria01

--- codigo/jsCod.js
+++ codigo/jsCod.js
@@ -1,11 +1,102 @@
-function abrir(){
-    let PequenoAlmoço = document.getElementById('pequenoAlmoço')
-    PequenoAlmoço.style.display="Block"
+function abrir1(){
+    let PequenoAlmoço = document.getElementById('pequenoAlmoço1')
+    PequenoAlmoço.style.display="block"
+
+    let botaosubio = document.getElementById("subio1")
+    botaosubio.style.display = "none"
+    
+    let botaodesceu = document.getElementById("desceu1")
+    botaodesceu.style.display = "block"
 }
 
-function fechar(){
-    let PequenoAlmoço = document.getElementById('pequenoAlmoço')
+
+
+function fechar1(){
+    let PequenoAlmoço = document.getElementById('pequenoAlmoço1')
     PequenoAlmoço.style.display="none"
+
+    let botaosubio = document.getElementById("desceu1")
+    botaosubio.style.display = "none"
+    
+    let botaodesceu = document.getElementById("subio1")
+    botaodesceu.style.display = "block"
 }
+
+
+
+function abrir2(){
+    let PequenoAlmoço = document.getElementById('pequenoAlmoço2')
+    PequenoAlmoço.style.display="block"
+
+    let botaosubio = document.getElementById("subio2")
+    botaosubio.style.display = "none"
+    
+    let botaodesceu = document.getElementById("desceu2")
+    botaodesceu.style.display = "block"
+}
+
+
+function fechar2(){
+    let PequenoAlmoço = document.getElementById('pequenoAlmoço2')
+    PequenoAlmoço.style.display="none"
+
+    let botaosubio = document.getElementById("desceu2")
+    botaosubio.style.display = "none"
+    
+    let botaodesceu = document.getElementById("subio2")
+    botaodesceu.style.display = "block"
+}
+
+
+
+function abrir3(){
+    let PequenoAlmoço = document.getElementById('pequenoAlmoço3')
+    PequenoAlmoço.style.display="block"
+
+    let botaosubio = document.getElementById("subio3")
+    botaosubio.style.display = "none"
+    
+    let botaodesceu = document.getElementById("desceu3")
+    botaodesceu.style.display = "block"
+}
+
+
+function fechar3(){
+    let PequenoAlmoço = document.getElementById('pequenoAlmoço3')
+    PequenoAlmoço.style.display="none"
+
+    let botaosubio = document.getElementById("desceu3")
+    botaosubio.style.display = "none"
+    
+    let botaodesceu = document.getElementById("subio3")
+    botaodesceu.style.display = "block"
+}
+
+
+
+function abrir4(){
+    let PequenoAlmoço = document.getElementById('pequenoAlmoço4')
+    PequenoAlmoço.style.display="block"
+
+    let botaosubio = document.getElementById("subio4")
+    botaosubio.style.display = "none"
+    
+    let botaodesceu = document.getElementById("desceu4")
+    botaodesceu.style.display = "block"
+}
+
+
+function fechar4(){
+    let PequenoAlmoço = document.getElementById('pequenoAlmoço4')
+    PequenoAlmoço.style.display="none"
+
+    let botaosubio = document.getElementById("desceu4")
+    botaosubio.style.display = "none"
+    
+    let botaodesceu = document.getElementById("subio4")
+    botaodesceu.style.display = "block"
+}
+
+
 
 
